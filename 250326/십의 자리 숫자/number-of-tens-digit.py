b245166945677f81a1efb37d354debf2c8311da9
TAEGYU,LEE <156196775+alphagyuu@users.noreply.github.com>
2025-03-26 20:03:30 +0900

    Title: 십의 자리 숫자, Time: 48ms, Memory: 16MB, Status: Passed - Codetree

--- 250326/십의 자리 숫자/number-of-tens-digit.py
+++ 250326/십의 자리 숫자/number-of-tens-digit.py
@@ -1,6 +1,12 @@
 def dec(n):
     return((n%100)//10)
 arr=list(map(int,input().split()))
-darr=[dec(a) for a in arr if a>9]
+darr=[]
+for a in arr:
+    if a==0:
+        break
+    elif a>9:
+        darr.append(dec(a))
+
 for i in range(1,10):
     print(f"{i} - {darr.count(i)}")
