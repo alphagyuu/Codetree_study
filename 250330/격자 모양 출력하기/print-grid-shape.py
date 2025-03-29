b0772815dbfcb48d8ec8528543d3bd391a94dae8
TAEGYU,LEE <156196775+alphagyuu@users.noreply.github.com>
2025-03-30 02:09:04 +0900

    Title: 격자 모양 출력하기, Time: 41ms, Memory: 16MB, Status: Passed - Codetree

--- 250330/격자 모양 출력하기/print-grid-shape.py
+++ 250330/격자 모양 출력하기/print-grid-shape.py
@@ -4,8 +4,8 @@ point_pos=[
     for _ in range(m)
 ]
 grid=[
-    [0 for j in range(m)]
-    for i in range(m)
+    [0 for j in range(n)]
+    for i in range(n)
 ]
 for point in point_pos:
     grid[point[0]-1][point[1]-1]=point[0]*point[1]
