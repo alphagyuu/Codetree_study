63ba12d1eb7d37d1f436b05dd847c576d8838dc6
TAEGYU,LEE <156196775+alphagyuu@users.noreply.github.com>
2025-03-30 23:52:41 +0900

    Title: 부분문자열 위치 구하기, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 250330/부분문자열 위치 구하기/find-location-of-substring.py
+++ 250330/부분문자열 위치 구하기/find-location-of-substring.py
@@ -4,12 +4,15 @@ pidx=0
 yes=False
 pos=0
 for i in range(len(word)):
+    if yes: 
+        break
     if word[i]==part[pidx]:
         pidx+=1
         if pidx==len(part):
             yes=True
             pos=i-len(part)+1
-            break
+    elif word[i]==part[0]:
+        pidx=1
     else:
         pidx=0
 if yes:
