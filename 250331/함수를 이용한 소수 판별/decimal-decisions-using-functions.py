e98adb199527e782b54f9eb1c6b06b9613354da6
TAEGYU,LEE <156196775+alphagyuu@users.noreply.github.com>
2025-03-31 13:03:19 +0900

    Title: 함수를 이용한 소수 판별, Time: 46ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250331/함수를 이용한 소수 판별/decimal-decisions-using-functions.py
@@ -0,0 +1,12 @@
+a,b=map(int,input().split())
+primes=[2]
+for i in range(3,b+1):
+    is_p=True
+    for p in primes:
+        if i%p==0:
+            is_p=False
+            break
+    if is_p:
+        primes.append(i)
+p_in_range=[p for p in primes if p>=a]
+print(sum(p_in_range))
