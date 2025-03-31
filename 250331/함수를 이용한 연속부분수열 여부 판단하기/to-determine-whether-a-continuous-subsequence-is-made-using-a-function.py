33ea9815ddb93297f9a6a90f64034afe2855946e
TAEGYU,LEE <156196775+alphagyuu@users.noreply.github.com>
2025-03-31 18:17:52 +0900

    Title: 함수를 이용한 연속부분수열 여부 판단하기, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 250331/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.py
+++ 250331/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.py
@@ -1,7 +1,9 @@
 ln1, ln2=map(int,input().split())
 alist=list(map(str,input().split()))
 blist=list(map(str,input().split()))
-if "".join(blist) in "".join(alist):
-    print("Yes")
-else:
-    print("No")
+def check(alist,blist):
+    for i in range(len(alist)-len(blist)+1):
+        if blist==alist[i:i+len(blist)]:
+            return "Yes"
+    return "No"
+print(check(alist,blist))
