6e4f6957f3ae7994edce9cb622000c58d74167f3
TAEGYU,LEE <156196775+alphagyuu@users.noreply.github.com>
2025-04-03 00:14:04 +0900

    Title: 최대로 겹치는 구간, Time: 47ms, Memory: 16MB, Status: Passed - Codetree

--- 250403/최대로 겹치는 구간/maximum-overlapped-segments.py
+++ 250403/최대로 겹치는 구간/maximum-overlapped-segments.py
@@ -10,7 +10,7 @@ for line in lines:
         axis=[0]*(first-line[0])+axis
         first=line[0]
     if last<line[1]:
-        axis=[0]*(line[1]-last)+axis
+        axis=axis+[0]*(line[1]-last)
         last=line[1]
     axis[line[0]-first:line[1]-first]=[x+1 for x in axis[line[0]-first:line[1]-first]]
 print(max(axis))
