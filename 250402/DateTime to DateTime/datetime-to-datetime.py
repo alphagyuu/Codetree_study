a42a04078de76db098e75a2bf30ad80e84893c25
TAEGYU,LEE <156196775+alphagyuu@users.noreply.github.com>
2025-04-02 23:03:29 +0900

    Title: DateTime to DateTime, Time: 43ms, Memory: 16MB, Status: Passed - Codetree

--- 250402/DateTime to DateTime/datetime-to-datetime.py
+++ 250402/DateTime to DateTime/datetime-to-datetime.py
@@ -3,9 +3,9 @@ def bun(d,h,m):
     return (d*60*24+h*60+m)
 if a<11:
     print(-1)
-elif b<11:
+elif a==11 and b<11:
     print(-1)
-elif c<11:
+elif a==11 and b==11 and c<11:
     print(-1)
 else:
     print(bun(a,b,c)-bun(11,11,11))
