ff4f794691e4bfbfb675a3c4737f176f703808de
TAEGYU,LEE <156196775+alphagyuu@users.noreply.github.com>
2025-04-02 09:33:26 +0900

    Title: 2개씩 그룹짓기, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

--- 250402/2개씩 그룹짓기/group-of-pairs.py
+++ 250402/2개씩 그룹짓기/group-of-pairs.py
@@ -1,4 +1,7 @@
 lng=int(input())
 items=list(map(int,input().split()))
-print(items.pop())
-print(items)
+items.sort()
+groups=[]
+for i in range(lng):
+    groups.append(items.pop(0)+items.pop())
+print(max(groups))
