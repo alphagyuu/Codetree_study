fb7a36683521e0cacf06fa1dcfa52c77d15f6ac0
TAEGYU,LEE <156196775+alphagyuu@users.noreply.github.com>
2025-04-02 22:26:09 +0900

    Title: DateTime to DateTime, Time: 43ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250402/DateTime to DateTime/datetime-to-datetime.py
+++ 250402/DateTime to DateTime/datetime-to-datetime.py
@@ -1,4 +1,11 @@
 a,b,c=map(int,input().split())
 def bun(d,h,m):
     return (d*60*24+h*60+m)
-print(bun(a,b,c)-bun(11,11,11))
+if a<11:
+    print(-1)
+elif b<11:
+    print(-1)
+elif c<11:
+    print(-1)
+else:
+    print(bun(a,b,c)-bun(11,11,11))
