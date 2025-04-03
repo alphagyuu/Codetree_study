fdcf1b1c5d0edeac6d484fbad2abc46d58a62fb0
TAEGYU,LEE <156196775+alphagyuu@users.noreply.github.com>
2025-04-04 01:00:27 +0900

    Title: 벌금은 누구에게, Time: 43ms, Memory: 16MB, Status: Passed - Codetree

--- 250404/벌금은 누구에게/who-will-pay.py
+++ 250404/벌금은 누구에게/who-will-pay.py
@@ -1,8 +1,12 @@
 ln,lm,k=map(int,input().split())
 students=[0]*(ln+1)
+find=False
 for _ in range(lm):
     n=int(input())
     students[n]+=1
     if students[n]>=k:
         print(n)
+        find=True
         break
+if not find:
+    print(-1)
