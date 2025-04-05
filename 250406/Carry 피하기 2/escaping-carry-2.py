7518967dcc758771b56d54d02dec4ed4b13ff95b
TAEGYU,LEE <156196775+alphagyuu@users.noreply.github.com>
2025-04-06 02:16:55 +0900

    Title: Carry 피하기 2, Time: 50ms, Memory: 17MB, Status: Passed - Codetree

--- 250406/Carry 피하기 2/escaping-carry-2.py
+++ 250406/Carry 피하기 2/escaping-carry-2.py
@@ -19,12 +19,13 @@ def carry_sum(i,j,k):
     return True
             
 fail=True
-for i in range(ln):
-    for j in range(i+1,ln):
-        for k in range(j+1,ln):
-            if carry_sum(i,j,k):
-                print(nums[i]+nums[j]+nums[k])
-                fail=False
-                break
-if fail:
+def loop(ln):
+    for i in range(ln):
+        for j in range(i+1,ln):
+            for k in range(j+1,ln):
+                if carry_sum(i,j,k):
+                    print(nums[i]+nums[j]+nums[k])
+                    return
     print(-1)
+    return
+loop(ln)
