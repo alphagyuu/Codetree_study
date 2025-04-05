97841be091e0c72e022e4908b24a73d3e314ffec
TAEGYU,LEE <156196775+alphagyuu@users.noreply.github.com>
2025-04-05 20:01:11 +0900

    Title: 작은 구슬의 이동, Time: 44ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250405/작은 구슬의 이동/small-marble-movement.py
@@ -0,0 +1,30 @@
+N,T=map(int,input().split())
+R,C,D=map(str,input().split())
+R=int(R)
+C=int(C)
+d=0
+if D=="U" or D=="D":
+    d= -1 if D=="U" else 1
+    for i in range(T):
+        if R==1 and D=="U":
+            d=1
+            D="D"
+        elif R==N:
+            d=-1
+            D="U"
+        else:
+            R+=d
+if D=="R" or D=="L":
+    d= -1 if D=="L" else 1
+    for i in range(T):
+        if C==1 and D=="L":
+            d=1
+            D="R"
+        elif C==N and D=="R":
+            d=-1
+            D="L"
+        else:
+            C+=d
+print(R,C)
+
+
