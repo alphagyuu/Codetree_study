45e1768df54ab542f358c5c0f40a6b0bcc065ee9
TAEGYU,LEE <156196775+alphagyuu@users.noreply.github.com>
2025-04-06 14:44:00 +0900

    Title: 숨은 단어 찾기 2, Time: 77ms, Memory: 18MB, Status: Passed - Codetree

--- 250406/숨은 단어 찾기 2/find-hidden-words-2.py
+++ 250406/숨은 단어 찾기 2/find-hidden-words-2.py
@@ -5,7 +5,7 @@ box= [
 ]
 
 def in_box(x,y):
-    if x>=0 and y>=0 and x<lm and x<ln:
+    if x>=0 and y>=0 and x<lm and y<ln:
         return True
     else:
         return False
@@ -17,20 +17,21 @@ lee=["L","E","E"]
 count=0
 for i in range(ln):
     for j in range(lm):
-        turn=0
-        x=i
-        y=i
         for dx,dy in zip(dxs,dys):
-            if box[y][x]==lee[turn]:
-                turn+=1
-            else:
-                turn=0
-            if turn==2:
-                count+=1
-                turn=0
-            if in_box(x+dx,y+dy):
-                x+=dx
-                y+=dy
-            else:
-                break
+            x=j
+            y=i
+            turn=0
+            for k in range(3):
+                if box[y][x]==lee[turn]:
+                    turn+=1
+                else:
+                    turn=0
+                if turn==3:
+                    count+=1
+                    turn=0
+                if in_box(x+dx,y+dy):
+                    x+=dx
+                    y+=dy
+                else:
+                    break
 print(count)
