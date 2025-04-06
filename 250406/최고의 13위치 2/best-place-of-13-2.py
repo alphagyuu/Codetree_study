e676f56adec41668fcf449dae51e456821f87614
TAEGYU,LEE <156196775+alphagyuu@users.noreply.github.com>
2025-04-06 15:09:59 +0900

    Title: 최고의 13위치 2, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

--- 250406/최고의 13위치 2/best-place-of-13-2.py
+++ 250406/최고의 13위치 2/best-place-of-13-2.py
@@ -11,12 +11,13 @@ for r in range(N):
         smallboxes.append([idx,sum(pan[r][c:c+3])])
         idx+=1
     idx+=2
-smallboxes.sort(key=lambda x:x[1],reverse=True)
+smallboxes.sort(key=lambda x:(x[1],x[0]),reverse=True)
 def loop():
     for i in range(len(smallboxes)):
         for j in range(i+1,len(smallboxes)):
             if abs(smallboxes[i][0]-smallboxes[j][0])>2:
                 return smallboxes[i][1]+smallboxes[j][1]
     return 0
+    
 print(loop())
 #print(smallboxes)
