e44229e23567dfb60974e19d07d3df75e3c4c654
TAEGYU,LEE <156196775+alphagyuu@users.noreply.github.com>
2025-04-06 17:37:09 +0900

    Title: 한 가지로 열리는 자물쇠, Time: 59ms, Memory: 17MB, Status: Passed - Codetree

--- 250406/한 가지로 열리는 자물쇠/one-way-lock.py
+++ 250406/한 가지로 열리는 자물쇠/one-way-lock.py
@@ -1,11 +1,12 @@
 N=int(input())
 comb=tuple(map(int,input().split()))
-keys=[]
+keys=0
 for i in range(1,N+1):
     for j in range(1,N+1):
-        keys=keys+[(c,i,j) for c in range((max(1,comb[0]-2)),min(N,comb[0]+2)+1)]
-        keys=keys+[(i,c,j) for c in range((max(1,comb[1]-2)),min(N,comb[1]+2)+1)] 
-        keys=keys+[(i,j,c) for c in range((max(1,comb[2]-2)),min(N,comb[2]+2)+1)]
-print(len(set(keys)))
+        for k in range(1,N+1):
+            if i in range((max(1,comb[0]-2)),min(N,comb[0]+2)+1) or j in range((max(1,comb[1]-2)),min(N,comb[1]+2)+1) or k in range((max(1,comb[2]-2)),min(N,comb[2]+2)+1):
+                keys+=1
+
+print(keys)
 
 
