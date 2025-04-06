2a6dc2d51f3dda0a8a8269ac06b064686989b6b2
TAEGYU,LEE <156196775+alphagyuu@users.noreply.github.com>
2025-04-06 13:51:57 +0900

    Title: 오목, Time: 71ms, Memory: 18MB, Status: Runtime Error - Codetree

---
+++ 250406/오목/O-mok.py
@@ -0,0 +1,93 @@
+pan=[
+    list(map(int,input().split()))
+    for _ in range(19)
+]
+
+def check():
+    before=pan[0][0]
+    combo=0
+    for i in range(19):
+        for j in range(19):
+            if pan[i][j] != 0:
+                if before==pan[i][j]:
+                    combo+=1
+                else:
+                    combo=1
+                if combo==5:
+                    return (before,i+1,j-1)
+            before=pan[i][j]
+    before=pan[0][0]
+    combo=0
+    for j in range(19):
+        for i in range(19):
+            if pan[i][j] != 0:
+                if before==pan[i][j]:
+                    combo+=1
+                else:
+                    combo=1
+                if combo==5:
+                    return (before,i-1,j+1)
+            before=pan[i][j]
+    before=pan[0][0]
+    combo=0
+    for i in range(0,15):
+        x=i
+        y=0
+        while x<19:
+            if pan[y][x]!=0:
+                if before==pan[y][x]:
+                    combo+=1
+                else:
+                    combo=1
+                if combo==5:
+                    return (before,x-1,y-1)
+            x+=1
+            y+=1
+    before=pan[1][0]
+    combo=0    
+    for i in range(1,15):
+        x=0
+        y=1
+        while y<19:
+            if pan[y][x]!=0:
+                if before==pan[y][x]:
+                    combo+=1
+                else:
+                    combo=1
+                if combo==5:
+                    return (before,x-1,y-1)
+            x+=1
+            y+=1
+    before=pan[0][4]
+    combo=0
+    for i in range(4,19):
+        x=i
+        y=0
+        while x>=0:
+            if pan[y][x]!=0:
+                if before==pan[y][x]:
+                    combo+=1
+                else:
+                    combo=1
+                if combo==5:
+                    return (before,x+3,y-1)
+            x-=1
+            y+=1                         
+    for i in range(1,15):
+        x=18
+        y=i
+        while x>=0:
+            if pan[y][x]!=0:
+                if before==pan[y][x]:
+                    combo+=1
+                else:
+                    combo=1
+                if combo==5:
+                    return (before,x+3,y-1)
+            x-=1
+            y+=1    
+
+out=check()
+print(out[0])
+print(*out[1:])        
+        
