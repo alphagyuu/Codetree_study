6681c9bb8dadab8700144c305f287345b2f72e39
TAEGYU,LEE <156196775+alphagyuu@users.noreply.github.com>
2025-04-07 12:32:13 +0900

    Title: 야바위, Time: 45ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250407/야바위/ya-rock.py
@@ -0,0 +1,18 @@
+N=int(input())
+turns=[
+    list(map(int,input().split()))
+    for _ in range(N)
+]
+for i in range(3):
+    cups=[0,0,0,0]
+    cups[i]=1
+    cnt=0
+    max_cnt=0
+    for a,b,c in turns:
+        cups[a],cups[b]=cups[b],cups[a]
+        if cups[c]==1:
+            cnt+=1
+    if max_cnt<cnt:
+        max_cnt=cnt
+print(max_cnt)
+
