58288fe39f30bc3a3c4c48e5302c009a5f81f213
TAEGYU,LEE <156196775+alphagyuu@users.noreply.github.com>
2025-04-08 01:30:51 +0900

    Title: 움직이는 블록, Time: 85ms, Memory: 18MB, Status: Passed - Codetree

--- 250408/움직이는 블록/moving-block.py
+++ 250408/움직이는 블록/moving-block.py
@@ -4,8 +4,15 @@ boxes=[
     for _ in range(N)
 ]
 cnt=0
+avg=sum(boxes)//len(boxes)
+'''
 while max(boxes)>min(boxes):
     boxes[boxes.index(max(boxes))]-=1
     boxes[boxes.index(min(boxes))]+=1
     cnt+=1
+시간초과
+'''
+for v in boxes:
+    if v>avg:
+        cnt+=v-avg
 print(cnt)
