56d3c2c27796cf36907167f573cd06f6daf28728
TAEGYU,LEE <156196775+alphagyuu@users.noreply.github.com>
2025-04-08 00:31:11 +0900

    Title: 연속된 숫자 만들기 2, Time: 48ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250408/연속된 숫자 만들기 2/create-consecutive-numbers-2.py
+++ 250408/연속된 숫자 만들기 2/create-consecutive-numbers-2.py
@@ -1,18 +1,19 @@
 '''
 123 -> 0
 12 4 -> 1
-1 3 5 -> 2
+1 3 5 -> 1
+1 2 6 -> 2
 거리차이 set
-1 1 -> 0
+1 1 -> 1
 1 2
-2 2 하나라도 2이하이면 -> 1
+2 2 둘다 2이하이면 -> 1
 '''
 humans=list(map(int,input().split()))
 d1=humans[1]-humans[0]
 d2=humans[2]-humans[1]
 if d2==1 and d1==1:
     print(0)
-elif d1<=2 or d2<=2:
+elif d1<=2 and d2<=2:
     print(1)
 else:
     print(2)
