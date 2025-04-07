dc206de0248e6e561f568ce8bc18a8711d263757
TAEGYU,LEE <156196775+alphagyuu@users.noreply.github.com>
2025-04-07 18:37:46 +0900

    Title: 3개의 선 2, Time: 50ms, Memory: 16MB, Status: Passed - Codetree

--- 250407/3개의 선 2/three-lines-2.py
+++ 250407/3개의 선 2/three-lines-2.py
@@ -11,17 +11,23 @@ valid=0
 for i in range(len(xys)):
     for j in range(i+1,len(xys)):
         for k in range(j+1,len(xys)):
-            l=[['o','o']]*3
+            l=[['o','o'] for _ in range(3)]
             for idx,x in enumerate ((i,j,k)):
                 if x<len(xs):
                     l[idx][0]=xys[x]
                 else:
                     l[idx][1]=xys[x]
             cnt=0
+            #print(i,j,k)
+            #print(l)
             for dot in dots:
+                #print(dot)
+                inline=0
                 for line in l:
                     if dot[0]==line[0] or dot[1]==line[1]:
-                        cnt+=1
+                        inline=1
+                cnt+=inline
+                #print(inline)
             if cnt==len(dots):
                 valid=1
 
