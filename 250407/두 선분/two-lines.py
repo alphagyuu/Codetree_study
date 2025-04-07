7ba0e9bb365582dc79a5cb6a0df9286d75fcf262
TAEGYU,LEE <156196775+alphagyuu@users.noreply.github.com>
2025-04-07 22:34:56 +0900

    Title: 두 선분, Time: 40ms, Memory: 16MB, Status: Passed - Codetree

--- 250407/두 선분/two-lines.py
+++ 250407/두 선분/two-lines.py
@@ -2,7 +2,7 @@ x1,x2,x3,x4=map(int,input().split())
 
 def loop(x1,x2,x3,x4):
     for x in range(x1,x2+1):
-        if x>=x3 or x<=x4:
+        if x>=x3 and x<=x4:
             return("intersecting")
     return("nonintersecting")
 
