8684ec5d8d1fa682627e5b45a754bec06b8c8815
TAEGYU,LEE <156196775+alphagyuu@users.noreply.github.com>
2025-04-08 00:49:05 +0900

    Title: 독서실의 거리두기 2, Time: 47ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 250408/독서실의 거리두기 2/study-cafe-keeping-distance-2.py
@@ -0,0 +1,39 @@
+#N=int(input())
+intext="1000100100010"
+seats=[intext[i] for i in range(len(intext))]
+#print(seats)
+
+N=int(input())
+seats=input()
+#print(seats)
+seats=[seats[i] for i in range(len(seats))]
+
+max_combo=0
+for i in range(len(seats)):
+    if seats[i]=="1":
+        continue
+    else:
+        seats[i]="1"
+    combo=0
+    #print(seats)
+    min_combo=len(seats)
+    is_edge=True
+    for j in range(len(seats)):
+        if seats[j]=='0':
+            combo+=1
+        else:
+            if combo==0:
+                if j>0:
+                    min_combo=0
+                else:
+                    is_edge=False
+            elif min_combo>combo:
+                if is_edge:
+                    is_edge=False
+                else:
+                    min_combo=combo
+            combo=0
+    if min_combo>max_combo:
+        max_combo=min_combo
+    seats[i]="0"
+print(max_combo+1)
