bf6b85c4ff6db65fc6d85bc09931813c83d1681b
TAEGYU,LEE <156196775+alphagyuu@users.noreply.github.com>
2025-04-07 12:37:45 +0900

    Title: 야바위, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 250407/야바위/ya-rock.py
+++ 250407/야바위/ya-rock.py
@@ -3,11 +3,11 @@ turns=[
     list(map(int,input().split()))
     for _ in range(N)
 ]
+max_cnt=0
 for i in range(3):
     cups=[0,0,0,0]
-    cups[i]=1
+    cups[i+1]=1
     cnt=0
-    max_cnt=0
     for a,b,c in turns:
         cups[a],cups[b]=cups[b],cups[a]
         if cups[c]==1:
