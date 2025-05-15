2ff5981c876cc92529b000bf45aa3b721b424671
TAEGYU,LEE <156196775+alphagyuu@users.noreply.github.com>
2025-05-15 16:59:50 +0900

    Title: 달에서 무게 구하기, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250515/달에서 무게 구하기/weight-on-the-moon.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 13;
+    double b = 0.165000;
+    double result=a*b;
+    cout << fixed;
+    cout.precision(6);
+    cout << a << " * " << b << " = " << result;
+    return 0;
+}
