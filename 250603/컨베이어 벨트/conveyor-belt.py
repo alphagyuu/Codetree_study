6bfc52d65a835f44626b820cd9e488e0683c0ed7
TAEGYU,LEE <156196775+alphagyuu@users.noreply.github.com>
2025-06-03 17:04:24 +0900

    Title: 컨베이어 벨트, Time: 55ms, Memory: 16MB, Status: Passed - Codetree

--- 250603/컨베이어 벨트/conveyor-belt.py
+++ 250603/컨베이어 벨트/conveyor-belt.py
@@ -7,13 +7,13 @@ for _ in range(2):
 
 for _ in range(T):
     last=belt[-1]
-    for i in range(4,-1,-1):
+    for i in range(N*2-2,-1,-1):
         belt[i+1]=belt[i]
     belt[0]=last
 
 
-for i in range(3):
+for i in range(N):
     print(belt[i],end=" ")
 print("")
-for i in range(3,6):
+for i in range(N,2*N):
     print(belt[i],end=" ")
