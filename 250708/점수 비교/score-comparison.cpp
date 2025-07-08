0c0484f12a56ead50d7d65142102c32193c9a7bb
TAEGYU,LEE <156196775+alphagyuu@users.noreply.github.com>
2025-07-08 20:21:32 +0900

    Title: 점수 비교, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 250708/점수 비교/score-comparison.cpp
+++ 250708/점수 비교/score-comparison.cpp
@@ -3,11 +3,11 @@ using namespace std;
 
 int main() {
     int a1, a2, b1, b2;
-    cin >> a1 >> b1 >> a2 >> b2;
+    cin >> a1 >> a2 >> b1 >> b2;
     if(a1>b1 && a2>b2) {
         cout << 1;
     }
     else {
-        cout <<0;
+        cout << 0;
     }
 }
