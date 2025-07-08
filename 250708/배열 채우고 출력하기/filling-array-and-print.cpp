6da7b78a6f9481d395f96500249afac042dbbde3
TAEGYU,LEE <156196775+alphagyuu@users.noreply.github.com>
2025-07-08 20:41:24 +0900

    Title: 배열 채우고 출력하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250708/배열 채우고 출력하기/filling-array-and-print.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char arr[10];
+    int i;
+    for(i=0;i<10;i++) {
+        cin >> arr[i];
+    }
+    for(i=9;i>=0;i--) {
+        cout << arr[i];
+    }
+}
