a34018746a08bd009aab28b2abbbed75d23b6ad7
TAEGYU,LEE <156196775+alphagyuu@users.noreply.github.com>
2025-07-27 18:47:57 +0900

    Title: 낮은 지점들, Time: 53ms, Memory: 1MB, Status: Wrong Answer - Codetree

---
+++ 250727/낮은 지점들/lowest-points.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <unordered_map>
+
+using namespace std;
+
+unordered_map<int,int> dots;
+
+int main(){
+    int N;
+    cin >> N;
+    int x,y,i;
+    for(i=0;i<N;i++) {
+        cin >> x >> y;
+        if(dots.find(x) != dots.end()) {
+            dots[x]=y;
+        }
+        else {
+            dots[x]=min(dots[x],y);
+        }
+    }
+    int tot=0;
+    for(unordered_map<int,int>::iterator it = dots.begin(); it!=dots.end(); it++) {
+        tot+=it->second;
+    }
+    cout << tot;
+    return 0;
+}
