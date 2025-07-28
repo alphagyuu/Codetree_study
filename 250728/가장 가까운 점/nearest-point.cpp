ecd29e7eae5ccb621f3cc391472fb0a8a038e8dc
TAEGYU,LEE <156196775+alphagyuu@users.noreply.github.com>
2025-07-28 22:37:37 +0900

    Title: 가장 가까운 점, Time: 7ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250728/가장 가까운 점/nearest-point.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <queue>
+#include <tuple>
+using namespace std;
+
+int main() {
+    int n, m, i, x, y;
+    cin >> n >> m;
+    priority_queue<tuple<int,int,int>> pq;
+    for(i=0;i<n;i++) {
+        cin >> x >> y;
+        pq.push(make_tuple(-(x+y),x,y));
+    }
+    for(i=0;i<m;i++) {
+        tuple<int,int,int> topdot = pq.top();
+        pq.pop();
+        pq.push(make_tuple(get<0>(topdot)-4, get<1>(topdot)+2, get<2>(topdot)+2));
+    }
+    tuple<int,int,int> topdot = pq.top();
+    cout << get<1>(topdot) << " " << get<2>(topdot);
+    return 0;
+}
