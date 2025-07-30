e05e220692b380d21816f654e8e81dfaa4d536ff
TAEGYU,LEE <156196775+alphagyuu@users.noreply.github.com>
2025-07-30 22:38:10 +0900

    Title: 최솟값 3개, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250730/최솟값 3개/top-3-smallest-number.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+priority_queue<int> pq;
+int main() {
+    int n, x, i, a, b, c;
+    cin >> n;
+    for(i=0;i<n;i++) {
+        cin >> x;
+        pq.push(-x);
+        if(i<2) {
+            cout << -1 << "\n";
+            continue;
+        }
+        a = -pq.top();
+        pq.pop();
+        b = -pq.top();
+        pq.pop();
+        c = -pq.top();
+        cout << a*b*c << "\n";
+        pq.push(-a);
+        pq.push(-b);
+    }
+    return 0;
+}
