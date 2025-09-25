9dc3b03cf9ffc4139e172f58f51df8456f66779d
TAEGYU,LEE <156196775+alphagyuu@users.noreply.github.com>
2025-09-25 16:41:45 +0900

    Title: 최소 정수 출력, Time: 348ms, Memory: 29MB, Status: Passed - Codetree

---
+++ 250925/최소 정수 출력/min-integer-output.py
@@ -0,0 +1,15 @@
+import heapq
+
+N=int(input())
+
+pq=[]
+
+for _ in range(N):
+    n=int(input())
+    if n==0:
+        if not pq:
+            print(0)
+        else:
+            print(heapq.heappop(pq))
+    else:
+        heapq.heappush(pq,n)
