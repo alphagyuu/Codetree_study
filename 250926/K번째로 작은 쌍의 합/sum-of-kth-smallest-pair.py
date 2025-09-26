93ece6bee4ee402b5339b84c5790a71d86e9f32e
TAEGYU,LEE <156196775+alphagyuu@users.noreply.github.com>
2025-09-26 10:56:10 +0900

    Title: K번째로 작은 쌍의 합, Time: 2089ms, Memory: 26MB, Status: Time Limit Exceed - Codetree

--- 250926/K번째로 작은 쌍의 합/sum-of-kth-smallest-pair.py
+++ 250926/K번째로 작은 쌍의 합/sum-of-kth-smallest-pair.py
@@ -6,31 +6,12 @@ ns=list(map(int,input().split()))
 
 ms=list(map(int,input().split()))
 
-heapq.heapify(ns)
-heapq.heapify(ms)
-
-ncnt=1
-mcnt=1
-tot=1
-seln=[heapq.heappop(ns)]
-selm=[heapq.heappop(ms)]
-
-while tot<K:
-    if not ns or (ms and (ms[0]+seln[0])<(ns[0]+selm[0])):
-        selm.append(heapq.heappop(ms))
-        mcnt+=1
-    else:
-        seln.append(heapq.heappop(ns))
-        ncnt+=1
-    tot=mcnt*ncnt
-
 pq=[]
 
-for i in range(ncnt):
-    for j in range(mcnt):
-        heapq.heappush(pq,seln[i]+selm[j])
+for n in ns:
+    for m in ms:
+        heapq.heappush(pq,-(n+m))
+        if len(pq)>K:
+            heapq.heappop(pq)
 
-for _ in range(K-1):
-    heapq.heappop(pq)
-
-print(pq[0])
+print(-pq[0])
