6ed40b236234a6bc27aabdbc29752cbec30cd518
TAEGYU,LEE <156196775+alphagyuu@users.noreply.github.com>
2025-10-09 23:17:10 +0900

    Title: 점 개수 세기 3, Time: 266ms, Memory: 23MB, Status: Passed - Codetree

--- 251009/점 개수 세기 3/count-number-of-points-3.py
+++ 251009/점 개수 세기 3/count-number-of-points-3.py
@@ -1,15 +1,10 @@
-from sortedcontainers import SortedSet
-
 N,Q = map(int,input().split())
 
 ns = list(map(int,input().split()))
 
-s = SortedSet()
-
 n2i=dict()
 
 for i,n in enumerate(ns):
-    s.add(i)
     n2i[n]=i
 
 for _ in range(Q):
