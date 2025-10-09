3619c15b4d80da1fc9dcc5ead12221e0c0861d64
TAEGYU,LEE <156196775+alphagyuu@users.noreply.github.com>
2025-10-09 22:08:06 +0900

    Title: 부분 수열의 합이 K, Time: 53ms, Memory: 17MB, Status: Passed - Codetree

--- 251009/부분 수열의 합이 K/the-sum-of-the-subsequences-is-k.py
+++ 251009/부분 수열의 합이 K/the-sum-of-the-subsequences-is-k.py
@@ -10,10 +10,8 @@ for i in range(2,N+1):
 cnt=0
 
 for i in range(1,N+1):
-    if prefix_sums[i]==K:
-        cnt+=1
-    elif prefix_sums[i]>K:
-        for j in range(2,i):
+    if prefix_sums[i]>=K:
+        for j in range(1,i+1):
             partial_sum=prefix_sums[i]-prefix_sums[j-1]
             if partial_sum==K:
                 cnt+=1
