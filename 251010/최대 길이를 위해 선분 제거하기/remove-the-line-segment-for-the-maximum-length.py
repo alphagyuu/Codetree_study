785d74c8c5ca5c0bdf5be6862267b53c0436cbe3
TAEGYU,LEE <156196775+alphagyuu@users.noreply.github.com>
2025-10-10 14:54:44 +0900

    Title: 최대 길이를 위해 선분 제거하기, Time: 669ms, Memory: 69MB, Status: Passed - Codetree

--- 251010/최대 길이를 위해 선분 제거하기/remove-the-line-segment-for-the-maximum-length.py
+++ 251010/최대 길이를 위해 선분 제거하기/remove-the-line-segment-for-the-maximum-length.py
@@ -31,6 +31,7 @@ for i in range(N):
 independent_length = [0]*(len(xs)) 
 total_length = 0
 gyeop = 0
+ps_independent_length = [0]*(len(xs))
 
 for i in range(len(xs)-1):
     gyeop += pm[i]
@@ -38,13 +39,14 @@ for i in range(len(xs)-1):
     if gyeop>0:
         total_length+=part_length
     if gyeop==1:
-        independent_length[i] = part_length
+        independent_length[i+1] = part_length
+    ps_independent_length[i+1] = ps_independent_length[i] + independent_length[i+1]
 
 min_independent_length = 1000000000
 
 for i in range(N):
     x1,x2 = lines_comp[i]
-    min_independent_length = min(min_independent_length,sum(independent_length[x1:x2]))
+    min_independent_length = min(min_independent_length,ps_independent_length[x2]-ps_independent_length[x1])
     #print(min_independent_length)
 
 print(total_length - min_independent_length)
