eb4c90203ed3242985d231d3752d82796dd3d2e7
TAEGYU,LEE <156196775+alphagyuu@users.noreply.github.com>
2025-10-10 14:49:14 +0900

    Title: 최대 길이를 위해 선분 제거하기, Time: 3095ms, Memory: 107MB, Status: Time Limit Exceed - Codetree

--- 251010/최대 길이를 위해 선분 제거하기/remove-the-line-segment-for-the-maximum-length.py
+++ 251010/최대 길이를 위해 선분 제거하기/remove-the-line-segment-for-the-maximum-length.py
@@ -1,54 +1,50 @@
 N = int(input())
 
 lines = []
-xs = []
+xs = set()
 
 for _ in range(N):
     x1,x2 = map(int,input().split())
-    xs+=[x1,x2]
+    xs.add(x1)
+    xs.add(x2)
     lines.append((x1,x2))
-
+    
+xs = list(xs)
 xs.sort()
 lines.sort()
 x = dict()
 xreal = dict()
 
-for i in range(N*2):
+for i in range(len(xs)):
     x[xs[i]] = i
     xreal[i] = xs[i]
 
-
-
-pm1 = [0]*(N*2)
-
-sx_2_li = dict()
-ex_2_li = dict()
 lines_comp = []
+pm = [0]*(len(xs))
 
 for i in range(N):
     x1,x2 = lines[i]
     lines_comp.append((x[x1],x[x2]))
-    pm1[x[x1]] = 1
-    pm1[x[x2]] = -1
-    sx_2_li[x[x1]] = i
-    ex_2_li[x[x2]] = i
+    pm[x[x1]]+=1
+    pm[x[x2]]-=1
 
-independent_length = [0]*(N*2)
+independent_length = [0]*(len(xs)) 
+total_length = 0
+gyeop = 0
 
-gyeop = 1
-tot_length = 0
-
-for i in range(1,N*2):
+for i in range(len(xs)-1):
+    gyeop += pm[i]
+    part_length = xreal[i+1]-xreal[i]
     if gyeop>0:
-        tot_length+=xreal[i]-xreal[i-1]
+        total_length+=part_length
     if gyeop==1:
-        independent_length[i] = xreal[i] - xreal[i-1]
-    gyeop+=pm1[i]
+        independent_length[i] = part_length
 
-min_independent_line = 1000000000
+min_independent_length = 1000000000
 
 for i in range(N):
     x1,x2 = lines_comp[i]
-    min_independent_line = min(min_independent_line,sum(independent_length[x1:x2]))
+    min_independent_length = min(min_independent_length,sum(independent_length[x1:x2]))
+    #print(min_independent_length)
 
-print(tot_length-min_independent_line)
+print(total_length - min_independent_length)
