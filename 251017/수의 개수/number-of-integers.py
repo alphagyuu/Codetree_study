0bb273216399795264d5a7b4b61890a892fbfe2a
TAEGYU,LEE <156196775+alphagyuu@users.noreply.github.com>
2025-10-17 14:12:31 +0900

    Title: 수의 개수, Time: 269ms, Memory: 22MB, Status: Passed - Codetree

--- 251017/수의 개수/number-of-integers.py
+++ 251017/수의 개수/number-of-integers.py
@@ -1,5 +1,33 @@
 N,M = map(int,input().split())
 Ns = list(map(int,input().split()))
+
+def bisect_left(m):
+    left, right = 0, N-1
+    min_idx = N
+    while left <= right:
+        mid = (left+right)//2
+        if Ns[mid] >= m:
+            right = mid - 1
+            min_idx = min(mid, min_idx)
+        else:
+            left = mid + 1
+    return min_idx
+
+def bisect_right(m):
+    left, right = 0, N-1
+    min_idx = N
+    while left <= right:
+        mid = (left+right)//2
+        if Ns[mid] > m:
+            right = mid - 1
+            min_idx = min(mid,min_idx)
+        else:
+            left = mid + 1
+    return min_idx
+
+
 for _ in range(M):
     m = int(input())
-    print(Ns.count(m))
+    l = bisect_left(m)
+    r = bisect_right(m)
+    print(r-l)
