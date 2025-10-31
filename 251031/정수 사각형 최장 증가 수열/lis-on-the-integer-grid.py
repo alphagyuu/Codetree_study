de61de83fc035c0ccc11b46988c26028c44978b3
TAEGYU,LEE <156196775+alphagyuu@users.noreply.github.com>
2025-10-31 16:16:30 +0900

    Title: 정수 사각형 최장 증가 수열, Time: 4063ms, Memory: 82MB, Status: Time Limit Exceed - Codetree

--- 251031/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.py
+++ 251031/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.py
@@ -2,12 +2,12 @@ def main():
     n = int(input())
 
     grid = [list(map(int,input().split())) for _ in range(n)]
-
-    moves = [set() for _ in range(n*n+1)]
+    
+    moves = set()
 
     for r in range(n):
         for c in range(n):
-            moves[1].add((r,c))
+            moves.add((r,c))
 
     dr = [0,0,1,-1]
     dc = [1,-1,0,0]
@@ -17,17 +17,19 @@ def main():
 
     for turn in range(1,n*n+1):
         empty = True
-        for r,c in moves[turn]:
+        next_moves = set()
+        for r,c in moves:
             for d in range(4):
                 nr, nc = r+dr[d], c+dc[d]
                 if not in_grid(nr,nc):
                     continue
                 if grid[nr][nc] > grid[r][c]:
                     empty = False
-                    moves[turn+1].add((nr,nc))
+                    next_moves.add((nr,nc))
         if empty:
             print(turn)
             return
+        moves = next_moves
     print(n*n)
     return
 
