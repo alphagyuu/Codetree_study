65563604dd87493e563926cac2f164cb5ab5f896
TAEGYU,LEE <156196775+alphagyuu@users.noreply.github.com>
2025-11-10 21:25:28 +0900

    Title: 부분 수열의 합이 M, Time: 60ms, Memory: 17MB, Status: Passed - Codetree

--- 251110/부분 수열의 합이 M/the-sum-of-the-subsequences-is-m.py
+++ 251110/부분 수열의 합이 M/the-sum-of-the-subsequences-is-m.py
@@ -7,6 +7,8 @@ A = list(map(int,input().split()))
 dp = [MAX]*(M+1)
 
 for i in range(N):
+    if A[i]>M:
+        continue
     for j in range(M-A[i],-1,-1):
         if dp[j]<MAX:
             dp[A[i]+j] = min(dp[A[i]+j],dp[j]+1)
