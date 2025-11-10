62944e31738b9478e8286185801625519e09e114
TAEGYU,LEE <156196775+alphagyuu@users.noreply.github.com>
2025-11-10 16:17:22 +0900

    Title: 2차원 최대 증가 수열, Time: 90ms, Memory: 18MB, Status: Wrong Answer - Codetree

--- 251110/2차원 최대 증가 수열/longest-increasing-sequence-2d.py
+++ 251110/2차원 최대 증가 수열/longest-increasing-sequence-2d.py
@@ -6,8 +6,8 @@ dp = [[1]*M for _ in range(N)]
 
 ans = 0
 
-for r in range(N):
-    for c in range(M):
+for r in range(N-1):
+    for c in range(M-1):
         for i in range(r+1,N):
             for j in range(c+1,M):
                 if grid[i][j]>grid[r][c]:
