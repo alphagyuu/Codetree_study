e9e1a814b11a1fdacb361c086b60e83e3d04ad69
TAEGYU,LEE <156196775+alphagyuu@users.noreply.github.com>
2025-11-10 21:24:37 +0900

    Title: 부분 수열의 합이 M, Time: 84ms, Memory: 18MB, Status: Runtime Error - Codetree

---
+++ 251110/부분 수열의 합이 M/the-sum-of-the-subsequences-is-m.py
@@ -0,0 +1,15 @@
+MAX = 10001
+
+N, M = map(int,input().split())
+
+A = list(map(int,input().split()))
+
+dp = [MAX]*(M+1)
+
+for i in range(N):
+    for j in range(M-A[i],-1,-1):
+        if dp[j]<MAX:
+            dp[A[i]+j] = min(dp[A[i]+j],dp[j]+1)
+    dp[A[i]] = 1
+    
+print(dp[M] if dp[M] < MAX else -1)
