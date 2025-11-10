9c3704a4501c05cfd1c8158b600affade0912c32
TAEGYU,LEE <156196775+alphagyuu@users.noreply.github.com>
2025-11-10 17:09:51 +0900

    Title: 동전 거슬러주기, Time: 75ms, Memory: 19MB, Status: Runtime Error - Codetree

---
+++ 251110/동전 거슬러주기/coin-change.py
@@ -0,0 +1,21 @@
+N,M = map(int,input().split())
+
+MAX = M*2
+
+coins = list(map(int,input().split()))
+
+coins.sort()
+
+dp = [MAX]*(M+1)
+
+for i in coins:
+    dp[i] = 1
+
+for i in range(coins[0]+1,M+1):
+    min_path = MAX
+    for c in coins:
+        if (i-c)>0 and dp[i-c]<MAX:
+            min_path = min(min_path, dp[i-c]+1)
+    dp[i] = min(min_path,dp[i])
+
+print(dp[-1] if dp[-1]<MAX else -1)
