bdf98c1803682fd0d1f46b44af4e5f836a764bf9
TAEGYU,LEE <156196775+alphagyuu@users.noreply.github.com>
2026-03-16 17:20:06 +0900

    Title: 정수 분배하기, Time: 47ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 260316/정수 분배하기/distributing-integers.py
+++ 260316/정수 분배하기/distributing-integers.py
@@ -15,7 +15,7 @@ max_k = -1
 while left <= right:
     mid = (left + right) // 2
     if is_possible(mid):
-        max_k = max(mid, max_k)
+
         left = mid + 1
     else:
         right = mid - 1
