35421896be4b40ef19ac480981e4629c215c6019
TAEGYU,LEE <156196775+alphagyuu@users.noreply.github.com>
2026-03-16 21:21:20 +0900

    Title: 삼 오 무, Time: 46ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 260316/삼 오 무/three-five-moo.py
+++ 260316/삼 오 무/three-five-moo.py
@@ -1,12 +1,7 @@
 N = int(input())
 
-ns = [] 
-
-
-
 left = 0
 right = N*3 - 1
-cnt = 0
 
 def cal(x):
     return x - x//3 - x//5 + x//15
@@ -14,11 +9,9 @@ def cal(x):
 while left <= right:
     mid = (left + right) // 2
     tot = cal(mid)
-    if tot == N:
-        break
-    if tot < N:
-        left = mid + 1
-    else:
+    if tot >= N:
         right = mid - 1
+    else:
+        left = mid + 1
 
 print(mid)
