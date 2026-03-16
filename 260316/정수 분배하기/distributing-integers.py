a64b22112c4f848dbf79ebb457d499b42ae83358
TAEGYU,LEE <156196775+alphagyuu@users.noreply.github.com>
2026-03-16 17:20:55 +0900

    Title: 정수 분배하기, Time: 83ms, Memory: 18MB, Status: Runtime Error - Codetree

--- 260316/정수 분배하기/distributing-integers.py
+++ 260316/정수 분배하기/distributing-integers.py
@@ -9,13 +9,13 @@ N, M = map(int,input().split())
 ns = [int(input()) for _ in range(N)]
 
 left = 0
-right = sum(ns) // M
+right = sum(ns)
 max_k = -1
 
 while left <= right:
     mid = (left + right) // 2
     if is_possible(mid):
-
+        max_k = max(mid, max_k)
         left = mid + 1
     else:
         right = mid - 1
