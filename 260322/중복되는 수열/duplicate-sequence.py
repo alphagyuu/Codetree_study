4720f094639150e3a237530b9f47b1682a88edd8
TAEGYU,LEE <156196775+alphagyuu@users.noreply.github.com>
2026-03-23 01:05:25 +0900

    Title: 중복되는 수열, Time: 158ms, Memory: 30MB, Status: Passed - Codetree

--- 260322/중복되는 수열/duplicate-sequence.py
+++ 260322/중복되는 수열/duplicate-sequence.py
@@ -1,31 +1,41 @@
-n = int(input())
-sequences = [input() for _ in range(n)]
-
-class node:
+class Node:
     def __init__(self):
         self.is_end = False
-        self.children = dict()
+        self.children = {}
 
-nodes = dict()
+def insert_and_check(root, word):
+    cur = root
+    for char in word:
+        # 1. 가는 길에 이미 끝나는 단어가 있다면? (기존 단어가 내 접두사)
+        if cur.is_end:
+            return False
+        
+        if char not in cur.children:
+            cur.children[char] = Node()
+        cur = cur.children[char]
+    
+    # 2. 단어 삽입을 마쳤는데, 이 노드에 이미 자식이 있다면? (내가 기존 단어의 접두사)
+    if cur.children:
+        return False
+    
+    # 3. 단어 삽입을 마쳤는데, 이미 is_end가 True라면? (중복 단어 입력)
+    if cur.is_end:
+        return False
 
-nodes[""] = node()
+    cur.is_end = True
+    return True
 
+
+n = int(input())
+sequences = [input().strip() for _ in range(n)]
+
+root = Node()
 prefix_yes = 1
 
 for seq in sequences:
-    if seq in nodes: 
-        if not nodes[seq].is_end:
-            prefix_yes = 0
-    cur = ""
-    for c in seq:
-        if cur+c not in nodes[cur].children:
-            nodes[cur+c] = node()
-            nodes[cur].children[cur+c] = nodes[cur+c]
-        elif nodes[cur+c].is_end:
-            prefix_yes = 0
-        cur+=c
-    nodes[cur].is_end = True
-
-print(prefix_yes)
-    
+    if not insert_and_check(root, seq):
+        prefix_yes = 0
+        break  # 하나라도 발견되면 더 볼 필요 없음
         
+print(prefix_yes)
+
