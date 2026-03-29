8e5c33f43ae9a54196ed067dc747aa10490d5f12
TAEGYU,LEE <156196775+alphagyuu@users.noreply.github.com>
2026-03-29 23:52:58 +0900

    Title: 집합의 원소, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260329/집합의 원소/elements-of-a-set.py
+++ 260329/집합의 원소/elements-of-a-set.py
@@ -1,16 +1,31 @@
-n, m = map(int, input().split())
+import sys
+sys.setrecursionlimit(10**6)
+input = sys.stdin.readline
 
-parent = [i for i in range(n+1)]
+n, m = map(int, input().split())
+parent = [i for i in range(n + 1)]
+size = [1] * (n + 1)  # 각 집합의 원소 개수 저장
 
 def find(x):
-    if parent[x] == x:
-        return x
-    else:
-        return find(parent[x])
+    if parent[x] != x:
+        parent[x] = find(parent[x]) # 경로 압축
+    return parent[x]
+
+def union(a, b):
+    rootA = find(a)
+    rootB = find(b)
+    
+    if rootA != rootB:
+        if size[rootA] < size[rootB]:
+            parent[rootA] = rootB
+            size[rootB] += size[rootA] # 사이즈 갱신
+        else:
+            parent[rootB] = rootA
+            size[rootA] += size[rootB]
 
 for _ in range(m):
-    c, a, b = map(int,input().split())
+    c, a, b = map(int, input().split())
     if c == 0:
-        parent[b] = a
+        union(a, b)
     else:
         print(1 if find(a) == find(b) else 0)
