7d0db1cd202d9ea71ce1cd0bf4dd2d6509d9301d
TAEGYU,LEE <156196775+alphagyuu@users.noreply.github.com>
2026-08-31 13:45:10 +0900

    [Passed] 숫자 빠르게 찾기 (536ms, 23MB)

--- 260831/숫자 빠르게 찾기/find-number-fast.py
+++ 260831/숫자 빠르게 찾기/find-number-fast.py
@@ -3,10 +3,19 @@ import bisect
 N,M=map(int,input().split())
 arr=list(map(int,input().split()))
 
+def find(target):
+    l, r = 0,N-1
+    while l <= r:
+        mid =(l+r)//2
+        if arr[mid] == target:
+            return mid+1
+        elif arr[mid] > target:
+            r = mid - 1
+        else:
+            l = mid + 1
+    return -1
+
+
 for _ in range(M):
     x = int(input())
-    i = bisect.bisect_left(arr,x)
-    if i < N and arr[i] == x:
-        print(i+1)
-    else:
-        print(-1)
+    print(find(x))
