7b535344867d232b5c116b91dcbe1decb9ca6290
TAEGYU,LEE <156196775+alphagyuu@users.noreply.github.com>
2026-08-31 16:25:39 +0900

    [Passed] 최대 감소 부분 수열 (68ms, 17MB)

---
+++ 260831/최대 감소 부분 수열/longest-decreasing-subsequence.py
@@ -0,0 +1,11 @@
+n = int(input())
+arr = list(map(int,input().split()))
+
+dp = [1]*n
+
+for i in range(n):
+    for j in range(i,n):
+        if arr[j] < arr[i]:
+            dp[j] = max(dp[i]+1,dp[j])
+
+print(max(dp))
