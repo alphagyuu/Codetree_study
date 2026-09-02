32a5cf910b6979d4212eb9f7d376f804aad0aa75
TAEGYU,LEE <156196775+alphagyuu@users.noreply.github.com>
2026-09-02 10:49:29 +0900

    [Passed] 집합의 원소 (757ms, 25MB)

---
+++ 260902/집합의 원소/elements-of-a-set.py
@@ -0,0 +1,29 @@
+n, m = map(int, input().split())
+parent = [i for i in range(n + 1)]
+size = [1] * (n + 1)  # 각 집합의 원소 개수 저장
+
+def find(x):
+    if parent[x] != x:
+        parent[x] = find(parent[x]) # 경로 압축
+    return parent[x]
+
+def union(a,b):
+    roota = find(a)
+    rootb = find(b)
+    if roota != rootb:
+        if size[roota] < size[rootb]:
+            parent[roota] = rootb
+            size[roota] += size[rootb]
+        else:
+            parent[rootb] = roota
+            size[rootb] += size[roota]
+
+for _ in range(m):
+    type,a,b = map(int,input().split())
+    if type == 0:
+        union(a,b)
+    else:
+        if find(a) == find(b):
+            print(1)
+        else:
+            print(0)
